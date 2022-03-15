e22ef46bb43340cf7c434baca88f1085e34e14b6
flytocc <flytocc@outlook.com>
2022-03-15 17:20:56 +0800

    equal area hbb

--- datasets/loaf.py
+++ datasets/loaf.py
@@ -6,35 +6,7 @@ Mostly copy-paste from https://github.com/pytorch/vision/blob/13b35ff/references
 """
 from pathlib import Path
 
-import math
 from .coco import CocoDetection, make_coco_transforms
-
-
-def poly_to_hbb(poly):
-    return [min(poly[0::2]), min(poly[1::2]), max(poly[0::2]), max(poly[1::2])]
-
-
-def rbbox_to_poly(rbbox):
-    xc, yc, w, h, alpha = rbbox
-    p1 = [- w / 2, - h / 2]
-    p2 = [  w / 2, - h / 2]
-    p3 = [  w / 2,   h / 2]
-    p4 = [- w / 2,   h / 2]
-
-    alpha_rad = - alpha / 180.
-    cos_theta, sin_theta = math.cos(alpha_rad), math.sin(alpha_rad)
-
-    rp1 = [p1[0] * cos_theta + p1[1] * sin_theta + xc,
-           p1[1] * cos_theta - p1[0] * sin_theta + yc]
-    rp2 = [p2[0] * cos_theta + p2[1] * sin_theta + xc,
-           p2[1] * cos_theta - p2[0] * sin_theta + yc]
-    rp3 = [p3[0] * cos_theta + p3[1] * sin_theta + xc,
-           p3[1] * cos_theta - p3[0] * sin_theta + yc]
-    rp4 = [p4[0] * cos_theta + p4[1] * sin_theta + xc,
-           p4[1] * cos_theta - p4[0] * sin_theta + yc]
-    poly = rp1 + rp2 + rp3 + rp4
-
-    return poly
 
 
 def build(image_set, args):
@@ -50,7 +22,7 @@ def build(image_set, args):
     dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
 
     for ann in dataset.coco.dataset['annotations']:
-        x0, y0, x1, y1 = poly_to_hbb(rbbox_to_poly(ann['bbox']))
-        ann['bbox'] = [x0, y0, x1 - x0, y1 - y0]
+        xc, yc, w, h, a = ann['bbox']
+        ann['bbox'] = [xc - w / 2, yc - h / 2, w, h]
 
     return dataset
